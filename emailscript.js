71eefd0297566c9152ef9061a9d40142da36b815
Penna92 <giacomopenna92@gmail.com>
2022-03-29 16:56:16 +0200

    Modificata pagina index.html (aggiunti 2 link per le altre pagine) email.html : aggiunto input per inserire l'email e bottone per la verifica. Creata dadi.html col precedente contenuto di index.html.

--- emailscript.js
+++ emailscript.js
@@ -1,15 +1,26 @@
 // lista di chi può accedere
-const lista = ["giacomo@email.it", "carlo@email.it", "peppe@email.it", "nonna@email.it", "carla@email.it", "clara@email.it", "peppina@email.it", "pippo@email.it", "pluto@email.it", "paperino@email.it"];
+const lista = [
+  "giacomo@email.it",
+  "carlo@email.it",
+  "peppe@email.it",
+  "nonna@email.it",
+  "carla@email.it",
+  "clara@email.it",
+  "peppina@email.it",
+  "pippo@email.it",
+  "pluto@email.it",
+  "paperino@email.it",
+];
 
-// chiedere all'utente la propria e-mail
-let userEmail = prompt("Scrivi la tua e-mail:");
-
-//verificare che sia nella lista di chi può accedere
-if(lista.includes(userEmail)){
-    alert("e-mail valida!")
-} else{
-    alert("la tua e-mail non risulta valida: prova a ridigitarla correttamente")
-}
-
-
-
+// al click del bottone, verificare che l'e-mail inserita dall'utente faccia parte della lista di e-mail valide
+const btn = document.getElementById("button");
+btn.onclick = function () {
+  let userEmail = document.getElementById("exampleInputEmail1").value;
+  if (lista.includes(userEmail)) {
+    alert("e-mail valida!");
+  } else {
+    alert(
+      "la tua e-mail non risulta valida: prova a ridigitarla correttamente"
+    );
+  }
+};
